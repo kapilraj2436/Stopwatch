d83be482f2383c1ad85575a0d6a1d1653f8094b7
Kapilraj chouhan <kapilrajchouhan@gmail.com>
2020-10-22 16:54:01 +0530

    Files Added

---
+++ App.js
@@ -0,0 +1,56 @@
+import React, { useState, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-regular-svg-icons'
+
+import './App.css';
+
+const element = <FontAwesomeIcon icon={faClock} />
+
+const App = () => {
+  const [timer, setTimer] = useState(0)
+  const [isActive, setIsActive] = useState(false)
+  const [isPaused, setIsPaused] = useState(false)
+  const increment = useRef(null)
+
+  const handleStart = () => {
+    setIsActive(true)
+    setIsPaused(true)
+    increment.current = setInterval(() => {
+      setTimer((timer) => timer + 1)
+    }, 1000)
+  }
+
+  const handleReset = () => {
+    setIsPaused(true)
+    clearInterval(increment.current)
+    setIsActive(false)
+    setIsPaused(false)
+    setTimer(0)
+  }
+
+  const formatTime = () => {
+    const getSeconds = `0${(timer % 60)}`.slice(-2)
+    const minutes = `${Math.floor(timer / 60)}`
+    const getMinutes = `0${minutes % 60}`.slice(-2)
+
+    return `${getMinutes} : ${getSeconds}`
+  }
+
+  return (
+    <div className="app">
+      <h3>React Stopwatch {element}</h3>
+      <div className='stopwatch-card'>
+        <p>{formatTime()}</p>
+        <div className='buttons'>
+          {
+            !isActive && !isPaused ?
+              <button onClick={handleStart}>Start</button>
+              : <button onClick={handleReset} disabled={!isActive}>Stop</button>
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
